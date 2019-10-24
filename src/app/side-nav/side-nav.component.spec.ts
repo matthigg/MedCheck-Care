3d4989ed8e175b0fe3310e4093fa5b583867f5d1
matthigg <mhh129@gmail.com>
2019-10-24 11:12:29 -0400

    Fix testing bug in side-nav component

--- src/app/side-nav/side-nav.component.spec.ts
+++ src/app/side-nav/side-nav.component.spec.ts
@@ -1,13 +1,15 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { AppRoutingModule } from '../app-routing.module';
 import { LayoutModule } from '@angular/cdk/layout';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -37,6 +39,7 @@ describe('SideNavComponent', () => {
         MatSidenavModule,
         MatToolbarModule,
         ReactiveFormsModule,
+        RouterTestingModule,
       ]
     }).compileComponents();
   }));
