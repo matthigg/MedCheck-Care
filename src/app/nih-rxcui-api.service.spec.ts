93b52002e8f40d694943a611fe493762179777dc
matthigg <mhh129@gmail.com>
2019-10-28 14:53:54 -0400

    Update ~/src/app/nih-rxcui-api.service.spec.ts test to include HttpClientTestingModule

--- src/app/nih-rxcui-api.service.spec.ts
+++ src/app/nih-rxcui-api.service.spec.ts
@@ -1,9 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NihRxcuiApiService } from './nih-rxcui-api.service';
 import { TestBed } from '@angular/core/testing';
 
-import { NihRxcuiApiService } from './nih-rxcui-api.service';
-
 describe('NihRxcuiApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ],
+  }));
 
   it('should be created', () => {
     const service: NihRxcuiApiService = TestBed.get(NihRxcuiApiService);
--- src/app/nih-rxcui-api.service.ts
+++ src/app/nih-rxcui-api.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NihRxcuiApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Send a request to the NIH RxCUI API with the intention of retrieving RxCUI
+  // values for each medication name in a meds[] array.  jm   
+  fetchRxCUIApi(meds: string[]): {med: string, observable: Observable<object>}[] {
+    const rxCUIObservables: {med: string, observable: Observable<object>}[] = [];
+    meds.forEach(med => {
+      if (med) {
+        rxCUIObservables.push({
+          'med': med,
+          'observable': this.http.get('https://rxnav.nlm.nih.gov/REST/rxcui?name=' + med)
+        });
+      }
+    });
+    return rxCUIObservables;
+  }
 }
