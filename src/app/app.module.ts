ba301c7c8a1ca8df674d272c379e4101749b1eea
matthigg <mhh129@gmail.com>
2019-10-31 09:53:47 -0400

    Update 'about' component UI - use Angular Material Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NgModule } from '@angular/core';
 // Angular Material Modules
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -38,6 +39,7 @@ import { SvgSpriteSheetComponent } from './svg-sprite-sheet/svg-sprite-sheet.com
     HttpClientModule,
     LayoutModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
