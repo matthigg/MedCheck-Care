f86335c45ba3b4d9032123f44def8946be17a0d1
matthigg <mhh129@gmail.com>
2019-11-01 13:26:44 -0400

    Add gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,7 +10,13 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private titleService: Title) { }
+  constructor(private titleService: Title, public router: Router) { 
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-151360300-1', { 'page_path': event.urlAfterRedirects });
+      }
+    })
+  }
 
   setTitle(newTitle: string) {
     const newTitleModified: string = newTitle + ' - MedCheck.care | Drug Interactions Tool' 
